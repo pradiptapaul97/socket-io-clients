b8e8f780ab61b80e48bc7fd1fa1abfd4e03470b5
pradiptapaul97 <ppradipta65@gmail.com>
2022-11-05 22:55:39 +0530

    second push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
+import { useState,React  } from "react";
+import { io } from "socket.io-client"; //import socket
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import InputGroup from 'react-bootstrap/InputGroup';
+
+import Form from 'react-bootstrap/Form';
+const socket = io('http://localhost:4000');
+
+socket.on('connect', () => {
+  console.log(socket.id,"room");
+});
+
+socket.on('receive-messege',(messege)=>{
+  console.log(1);
+  displayMessege(messege);
+})
+
+let displayMessege = (messege,sender) => {
+  sender = (sender?(sender):"Friend")
+  console.log(messege,sender);
+  let div = document.createElement("div");
+  div.classList.add("containerMessege");
+  let p = document.createElement("p");
+  p.appendChild(document.createTextNode(`${sender} : ${messege}`));
+  div.appendChild(p);
+  document.getElementById("messege-container").append(div);
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
+  let [messege, setMessege] = useState("");
+  let [room, setRoom] = useState("");
+
+  let sendMessege = () =>{
+    console.log(2);
+    console.log(messege);
+    if(messege !== '')
+    {
+      socket.emit('send-messege',messege,room);
+      displayMessege(messege,'me');
+      messege = '';
+    }
+  }
+
+  let joinRoom = () => {
+    console.log(3);
+    console.log(room);
+    socket.emit('join-room',room,(messege)=>{ //send callback to receive inforation
+      displayMessege(messege);
+  });
+  }
+
+  return (<>
+  <Container>
+  <h2>Chat Messages</h2>
+
+  <div id="messege-container" aria-readonly style={{overflowY:"scroll", height:"400px", background:"gray"}}>
+    
+  </div>
+     <InputGroup className="mb-3">
+        <Form.Control
+          placeholder="Messege"
+          aria-label="Messege"
+          aria-describedby="basic-addon2"
+          name="messege"
+          onChange={(e) => setMessege(e.target.value)}
+        />
+        <Button variant="outline-secondary" id="button-addon2" onClick={sendMessege}>
+          Send
+        </Button>
+      </InputGroup>
+      <InputGroup className="mb-3">
+        <Form.Control
+          placeholder="Room"
+          aria-label="Room"
+          aria-describedby="basic-addon2"
+          name="room"
+          onChange={(e) => setRoom(e.target.value)}
+        />
+        <Button variant="outline-secondary" id="button-addon2" onClick={joinRoom}>
+          Join
+        </Button>
+      </InputGroup>
+      </Container>
+    </>);
 }
 
 export default App;
