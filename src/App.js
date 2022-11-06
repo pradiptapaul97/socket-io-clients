54a38ae8721e720ff3214e6037d090555af5294f
pradiptapaul97 <ppradipta65@gmail.com>
2022-11-06 19:05:24 +0530

    design change iage add

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import InputGroup from 'react-bootstrap/InputGroup';
-
+import Compressor from 'compressorjs';
 import Form from 'react-bootstrap/Form';
 //const socket = io('http://localhost:4000');
 const socket = io('https://pradipta-socket-server.herokuapp.com');
@@ -15,13 +15,17 @@ socket.on('connect', () => {
   //console.log(socket.id,"room");
 });
 
-socket.on('receive-messege',(messege)=>{
+socket.on('receive-messege',(messegeObj)=>{
   //console.log(1);
-  displayMessege(messege);
+  //console.log(messegeObj);
+  let blob = new Blob([messegeObj.body],{ type : messegeObj.type});
+  //console.log(blob);
+  messegeObj.blob = blob;
+  displayMessege(messegeObj);
 })
 
-let displayMessege = (messege,sender) => {
-  sender = (sender?(sender):"Friend")
+let displayMessege = (messegeObj,sender) => {
+  sender = (sender?(sender):"Friend");
   //console.log(messege,sender);
   let div = document.createElement("div");
   div.classList.add("containerMessege");
@@ -29,6 +33,7 @@ let displayMessege = (messege,sender) => {
   let span = document.createElement("SPAN");
   let img = document.createElement('img');
   img.style.width = '100%'
+  img.classList.add("img");
   if(sender === 'Friend')
   {
     span.classList.add("time-left");
@@ -43,9 +48,20 @@ let displayMessege = (messege,sender) => {
     div.classList.add("darker");
   }
   span.appendChild(document.createTextNode(new Date().toLocaleString()));
-  p.appendChild(document.createTextNode(`${messege}`));
+  p.appendChild(document.createTextNode(`${messegeObj.body}`));
   div.appendChild(img);
-  div.appendChild(p);
+  if(messegeObj.type === 'text')
+  {
+    div.appendChild(p);
+  }
+  else
+  {
+    let myImage = document.createElement('img');
+    myImage.style.width = '50%'
+    myImage.style.height = '50%'
+    myImage.src = URL.createObjectURL(messegeObj.blob);
+    div.appendChild(myImage);
+  }
   div.appendChild(span);
   document.getElementById("messege-container").append(div);
 }
@@ -64,15 +80,41 @@ function App() {
   
   let [messege, setMessege] = useState("");
   let [room, setRoom] = useState("");
+  let [file, setFile] = useState("");
 
   let sendMessege = () =>{
     //console.log(2);
     //console.log(messege);
+    let messegeObj = {
+      type:"text",
+      body: messege,
+    }
     if(messege !== '')
     {
-      socket.emit('send-messege',messege,room);
-      displayMessege(messege,'Me');
+      socket.emit('send-messege',messegeObj,room);
+      displayMessege(messegeObj,'Me');
       setMessege('')
+    }
+  }
+
+  let sendFile = () =>{
+    //console.log(2);
+    //console.log(messege);
+    if(file)
+    {
+      let messegeObj = {
+        type:"file",
+        body: file,
+        mimeType:file.type,
+        fileName: file.name
+      }
+      socket.emit('send-messege',messegeObj,room);
+  
+      let blob = new Blob([messegeObj.body],{ type : messegeObj.type});
+      messegeObj.blob = blob;
+      displayMessege(messegeObj,'Me');
+      setMessege('');
+      setFile();
     }
   }
 
@@ -80,17 +122,33 @@ function App() {
     //console.log(3);
     //console.log(room);
     socket.emit('join-room',room,(messege)=>{ //send callback to receive inforation
-      displayMessege(messege);
+      let messegeObj = {
+        type:"text",
+        body: messege,
+      }
+      displayMessege(messegeObj);
   });
+  }
+
+  let selectFile = (e) =>{
+    new Compressor(e.target.files[0], {
+      quality: 0.8, // 0.6 can also be used, but its not recommended to go below.
+      success: (compressedResult) => {
+        // compressedResult has the compressed file.
+        // Use the compressed file to upload the images to your server.        
+        setFile(compressedResult)
+      },
+    });
+    //console.log(e.target.files[0]);
   }
 
   return (<>
   <Container>
   <h2>Chat Messages</h2>
 
-  <div id="messege-container" aria-readonly style={{overflowY:"scroll", height:"500px", background:"gray"}}>
+  <Container id="messege-container" aria-readonly style={{overflowY:"scroll", height:"400px", background:"gray"}}>
     
-  </div>
+  </Container>
      <InputGroup className="mb-3"></InputGroup>
      <InputGroup className="mb-3">
         <Form.Control
@@ -102,6 +160,19 @@ function App() {
           onChange={(e) => setMessege(e.target.value)}
         />
         <Button variant="outline-secondary" id="button-addon2" onClick={sendMessege}>
+          Send
+        </Button>
+      </InputGroup>
+      <InputGroup className="mb-3">
+        <Form.Control
+          placeholder="File"
+          aria-label="File"
+          aria-describedby="basic-addon2"
+          name="file"
+          type="file"
+          onChange={(e)=>{selectFile(e)}}
+        />
+        <Button variant="outline-secondary" id="button-addon2" onClick={sendFile}>
           Send
         </Button>
       </InputGroup>
